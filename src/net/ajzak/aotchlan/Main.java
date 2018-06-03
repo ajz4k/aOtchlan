761df282db056c575830c8edcce8abf0b81464a7
ajzak <xajzak216@gmail.com>
2018-06-03 11:54:34 +0200

    Podstawa configu gotowa.

--- src/net/ajzak/aotchlan/Main.java
+++ src/net/ajzak/aotchlan/Main.java
@@ -5,13 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
     private static Main inst;
-    public ConfigManager configManager;
 
     public void onEnable(){
         Main.inst = this;
         saveDefaultConfig();
-        configManager = new ConfigManager(this);
-        configManager.load();
     }
 
     public static Main getInst() {
